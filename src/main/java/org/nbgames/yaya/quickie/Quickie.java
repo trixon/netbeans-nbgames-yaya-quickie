ebc8d1263b06c3e027f8348a429bb3e3982b90dd
Patrik Karlström <patrik@trixon.se>
2018-07-23 12:48:00 +0200

    Update license header

--- src/main/java/org/nbgames/yaya/quickie/Quickie.java
+++ src/main/java/org/nbgames/yaya/quickie/Quickie.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2017 Patrik Karlsson.
+/* 
+ * Copyright 2018 Patrik Karlström.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
- * @author Patrik Karlsson
+ * @author Patrik Karlström
  */
 @ServiceProvider(service = YayaGameProvider.class)
 public class Quickie extends YayaGameProvider {
